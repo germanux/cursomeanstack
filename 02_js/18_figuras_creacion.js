033c44c2d7d3473a7d5f4828254570bea49848ef
German <gcaballero@athagon.com>
2017-01-05 06:04:51 +0100

    Implementando vista V2. lo demás, sirve completamente

--- 02_js/18_figuras_creacion.js
+++ 02_js/18_figuras_creacion.js
@@ -2,10 +2,10 @@ var padreCalcRectangulo = document.getElementById("padre-Calc-Rectangulo");
 var padreCalcTriangulo = document.getElementById("padre-Calc-Triangulo");
 var padreCalcElipse = document.getElementById("padre-Calc-Elipse");
 
-var vistaRectangulo = new FiguraVista(padreCalcRectangulo, "Rectangulo");
-var vistaTriangulo = new FiguraVista(padreCalcTriangulo, "Triangulo");
-var vistaElipse = new FiguraVista(padreCalcElipse, "Elipse");
+var vistaRectangulo = new FiguraVistaV2(padreCalcRectangulo, "Rectangulo");
+var vistaTriangulo = new FiguraVistaV2(padreCalcTriangulo, "Triangulo");
+var vistaElipse = new FiguraVistaV2(padreCalcElipse, "Elipse");
 
 var controlRectangulo = new FiguraControlador(vistaRectangulo, modeloRectangulo);
-var controlTriangulo = new FiguraControlador(vistaTriangulo, modeloTrianguloIsosceles);
+var controlTriangulo = new FiguraControlador(vistaTriangulo, modeloTrianguloEquilatero);
 var controlElipse = new FiguraControlador(vistaElipse, modeloElipse);
