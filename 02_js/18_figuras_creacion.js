d66ad7b6ae6a401a35c36298097a393075b86b97
German <gcaballero@athagon.com>
2017-01-05 03:38:47 +0100

    El HTML no debe tener nada de JS

---
+++ 02_js/18_figuras_creacion.js
@@ -0,0 +1,9 @@
+
+    var padreCalc = document.getElementById("padreCalc");
+    var rectangulo = new Rectangulo(padreCalc);
+
+    var padreCalc2 = document.getElementById("padreCalc2");
+    var triguangulo = new Triangulo(padreCalc2);
+
+    var padreCalc3 = document.getElementById("padreCalc3");
+    var triguangulo = new Elipse(padreCalc3);
