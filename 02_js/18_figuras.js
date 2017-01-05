26c159f35c4a430f3ad1b662fec102f571f16284
German <gcaballero@athagon.com>
2017-01-05 01:25:34 +0100

    Ejemplo herencia JS: Figuras - Rectangulo - Triangulo

--- 02_js/18_figuras.js
+++ 02_js/18_figuras.js
@@ -1,28 +1,31 @@
-var Rectangulo = function (padreDom) {
+var Figura = function (padreDom) {
 	
+	// Añadimos el FORM al padreDom
 	this.formCalc = document.createElement("form");
 	padreDom.appendChild(this.formCalc);
-	
+	// Y los botones
 	this.alto = CreadorDOM.addInput(this.formCalc, "alto", "alto");
 	this.ancho = CreadorDOM.addInput(this.formCalc, "ancho", "ancho");
-	CreadorDOM.addButton(this, " AREA ", this.calculaArea);
 	this.resultado = CreadorDOM.addInput(this.formCalc, "Resultado", "resultado");
+	// El botón de calcular
+	CreadorDOM.addButton(this, " Calcular Área " , this.calculaArea);
+	// Y la caja con el resultado
 }
+var Rectangulo = function (padreDom) {
+	Figura.call(this, padreDom);
+}
+Rectangulo.prototype = Figura;
+
 Rectangulo.prototype.calculaArea = function() {
 	this.resultado.value =   parseInt(this.alto.value) 
 						   * parseInt(this.ancho.value);
 }
 
 var Triangulo = function (padreDom) {
-	
-	this.formCalc = document.createElement("form");
-	padreDom.appendChild(this.formCalc);
-	
-	this.alto = CreadorDOM.addInput(this.formCalc, "alto", "alto");
-	this.ancho = CreadorDOM.addInput(this.formCalc, "ancho", "ancho");
-	CreadorDOM.addButton(this, " AREA ", this.calculaArea);
-	this.resultado = CreadorDOM.addInput(this.formCalc, "Resultado", "resultado");
+	Figura.call(this, padreDom);
 }
+Triangulo.prototype = Figura;
+
 Triangulo.prototype.calculaArea = function() {
 	this.resultado.value =   parseInt(this.alto.value) 
 						   * parseInt(this.ancho.value) / 2;
