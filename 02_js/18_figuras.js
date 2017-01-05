f278df13bf883c417bdf8c771919f960d2f58b70
German <gcaballero@athagon.com>
2017-01-05 02:54:34 +0100

    Añadiendo una funcion callback que se llama al crearse la figura

--- 02_js/18_figuras.js
+++ 02_js/18_figuras.js
@@ -1,42 +1,50 @@
-var Figura = function (padreDom, nombre) {	
-	if (padreDom) {
-		// Añadimos el FORM al padreDom
-		this.formCalc = document.createElement("form");
-		padreDom.appendChild(this.formCalc);
-		// Las cajas de texto de entrada
-		this.alto = CreadorDOM.addInput(this.formCalc, "alto", "alto");
-		this.ancho = CreadorDOM.addInput(this.formCalc, "ancho", "ancho");
-		// La caja con el resultado
-		this.resultado = CreadorDOM.addInput(this.formCalc, "Área", "resultado");
-		// El botón de calcular
-		CreadorDOM.addButton(this, " Calcular Área " + nombre, this.calculaArea);		
-	}	
+var Figura = function(padreDom, nombre, callBackAlCrearse) {
+        if (padreDom) {
+            // Añadimos el FORM al padreDom
+            this.formCalc = document.createElement("form");
+            padreDom.appendChild(this.formCalc);
+            // Las cajas de texto de entrada
+            this.ancho = CreadorDOM.addInput(this.formCalc, "ancho", "ancho");
+            this.alto = CreadorDOM.addInput(this.formCalc, "alto", "alto");
+            // La caja con el resultado
+            this.resultado = CreadorDOM.addInput(this.formCalc, "Área", "resultado");
+            // El botón de calcular
+            CreadorDOM.addButton(this, " Calcular Área " + nombre, this.calculaArea);
+            callBackAlCrearse();
+        }
+    }
+    // Función para heredar de figura
+Figura.Heredar = function(funCalculaArea, nombre, callBackAlCrearse) {
+    var nombreFun = nombre;
+    var callBackAlCrearseFuncion = callBackAlCrearse;
+    var functionConstructora = function(padreDom) {
+        Figura.call(this, padreDom, nombreFun, callBackAlCrearseFuncion);
+    }
+    functionConstructora.prototype = new Figura;
+    functionConstructora.prototype.calculaArea = funCalculaArea;
+    return functionConstructora;
 }
-// Función para heredar de figura
-Figura.Heredar = function (funCalculaArea, nombre) {
-	var nombreFun = nombre;
-	var functionConstructora = function (padreDom) {
-		Figura.call(this, padreDom, nombreFun);
-	}
-	functionConstructora.prototype = new Figura;
-	functionConstructora.prototype.calculaArea = funCalculaArea;
-	return functionConstructora;
-}
-function calcularAreaRectangulo () {
-	this.resultado.value =   parseInt(this.alto.value) 
-						   * parseInt(this.ancho.value);
+
+function calcularAreaRectangulo() {
+    this.resultado.value = parseInt(this.alto.value) *
+        parseInt(this.ancho.value);
 };
-var Rectangulo = Figura.Heredar(calcularAreaRectangulo, "Rectangulo");
 
 function calcularAreaTriangulo() {
-	this.resultado.value =   parseInt(this.alto.value) 
-						   * parseInt(this.ancho.value) / 2;
+    this.resultado.value = parseInt(this.alto.value) *
+        parseInt(this.ancho.value) / 2;
 }
-var Triangulo = Figura.Heredar(calcularAreaTriangulo, "Triangulo");
 
 function calculaAreaElipse() {
-	this.resultado.value =   (parseInt(this.alto.value) / 2)
-						   * (parseInt(this.ancho.value) / 2)
-						   * Math.PI;
+    this.resultado.value = (parseInt(this.alto.value) / 2) *
+        (parseInt(this.ancho.value) / 2) *
+        Math.PI;
 }
-var Elipse = Figura.Heredar(calculaAreaElipse, "Elipse");
+
+var Rectangulo = Figura.Heredar(calcularAreaRectangulo, "Rectangulo", MiFuncionCallBackGenerica);
+var Triangulo = Figura.Heredar(calcularAreaTriangulo, "Triangulo", MiFuncionCallBackGenerica);
+var Elipse = Figura.Heredar(calculaAreaElipse, "Elipse", MiFuncionCallBackGenerica);
+
+function MiFuncionCallBackGenerica() {
+    alert("Yeeeeeepaaaaah, se ha creado!!");
+}
