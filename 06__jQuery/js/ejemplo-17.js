6a4dba54905fa03a613c3af94a844bf737f0e9c0
G03-PROF\Administrator <gcaballero@athagon.com>
2017-01-16 19:58:44 +0100

    Ejemplo jQuery Ajax.

--- 06__jQuery/js/ejemplo-17.js
+++ 06__jQuery/js/ejemplo-17.js
@@ -1,3 +1,7 @@
 $(document).ready(inicializarEventos);
 function inicializarEventos(){
+	$("#peticion_ajax").click(dameFichero);
+}
+function dameFichero() {
+	$("#contenido_de_ajax").load("ejemplo-17-fichero_alojado_en_servidor.txt");
 }
--- 06__jQuery/js/ejemplo-18.js
+++ 06__jQuery/js/ejemplo-18.js
@@ -1,3 +1,27 @@
 $(document).ready(inicializarEventos);
 function inicializarEventos(){
+	$("#peticion_ajax").click(peticionAjaxGenerica);
+}
+
+function peticionAjaxGenerica() {
+	var opcionesAjax = {
+		data: {
+			nombre: "Ruben", nivel: "medio"
+			},
+		// type: 		"GET",
+		dataType: 	"json",
+		url: 		"https://jsonplaceholder.typicode.com/users"
+	};
+	$.ajax(opcionesAjax)
+		.done(peticionCompletada)
+		.fail(peticionFallida);
+}
+function peticionCompletada(datosRecibidos, status, jqXHR) {
+	alert ("Peticion completada. Estado: " + status + " : " + datosRecibidos);
+	$("#contenido_de_ajax").html(datosRecibidos[1].username);
+}
+function peticionFallida(jqXHR, status, error) {
+	alert("Error al procesar");
+	console.log("Estado: " + status);
+	console("Error! " + error);
 }
