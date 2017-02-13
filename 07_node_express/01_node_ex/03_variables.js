9b5058b042ee17a50927782130493a3047f80d02
G03-PROF\Administrator <gcaballero@athagon.com>
2017-02-13 19:41:05 +0100

    Captura de variables

--- 07_node_express/01_node_ex/03_variables.js
+++ 07_node_express/01_node_ex/03_variables.js
@@ -1,12 +1,18 @@
-var miExpress = require("express");
-// Instanciar
-var app = miExpress();
+var express = require("express");
+var app = express();
 
-app.get("/reservas/ere*pil", (request, response) => {
-    console.log("Visto " + request.originalUrl + " Verbo: " + request.method);
-    response.send("Visto " + request.originalUrl + " Verbo: " + request.method);
+app.get("/clientes/:idCliente?/facturas/:idFactura",
+    (request, response) => {
+        response.send("Resultados : \n\tCliente: " +
+            request.params.idCliente + " \n\tFactura: " +
+            request.params.idFactura + "\n" +
+            JSON.stringify(request.params));
+    }
+);
+app.get("/save/:fichero.:extension", (request, response) => {
+    response.send("Ahora genero un fichero de tipo : " +
+        request.params.extension);
 });
-
 app.listen(9000);
 
-console.log("Servidor express lanzado");
+console.log("Servidor iniciado");
